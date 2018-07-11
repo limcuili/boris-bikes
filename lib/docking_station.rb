81d4578bad3c9c896e9c9c030638851795ab2e5f
limcuili <limcuili@hotmail.com>
2018-07-11 17:36:17 +0100

    Add dock(bike)

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -2,15 +2,21 @@ class DockingStation
   def release_bike
     Bike.new
   end
+
+  def dock(bike)
+    # To return the bike we dock,
+    @bike = bike
+  end
+
+  attr_reader :bike
+  # "attr_reader :bike" replaces the below method
+  # def bike
+  #   @bike
+  # end
 end
 
 class Bike
   def working?
     true
   end
-
 end
-
-station = DockingStation.new
-bike = station.release_bike
-bike.working?
