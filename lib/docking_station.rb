211d68cd2e0f29a869854551a31dec5390a2fd2b
limcuili <limcuili@hotmail.com>
2018-07-11 16:57:56 +0100

    Add 'release_bike' and 'working?' methods

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,3 +1,16 @@
 class DockingStation
+  def release_bike
+    Bike.new
+  end
+end
+
+class Bike
+  def working?
+    true
+  end
 
 end
+
+station = DockingStation.new
+bike = station.release_bike
+bike.working?
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,5 +1,10 @@
 require 'docking_station'
 
 describe DockingStation do
-
+	it { is_expected.to respond_to :release_bike}
+#	it { respond_to?(release_bike) }
+	it 'checks bike is working' do
+		bike = subject.release_bike
+		expect(bike).to be_working
+	end
 end
