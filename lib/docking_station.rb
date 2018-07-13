21a72c135f2757ea4f714916646b49821a0d7663
limcuili <limcuili@hotmail.com>
2018-07-13 16:25:29 +0100

    Allow varying capacity

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,18 +1,41 @@
 class DockingStation
-  def release_bike
-    Bike.new
+
+  DEFAULT_CAPACITY = 20
+
+  def initialize(capacity = 20)
+    @bikes = []
+    @capacity = capacity
   end
 
-  def dock(bike)
-    # To return the bike we dock,
-    @bike = bike
-  end
-
-  attr_reader :bike
+  attr_reader :bikes
+  attr_reader :capacity
   # "attr_reader :bike" replaces the below method
   # def bike
   #   @bike
   # end
+
+  def release_bike
+    fail 'No bikes available' if empty?
+    @bikes.pop
+  end
+
+  def dock(bike)
+    fail 'Docking station full' if full?
+    # To return the bike we dock
+    @bikes << bike
+    @bikes[@bikes.size - 1]
+  end
+
+
+private
+  def full?
+    !!(@bikes.size >= @capacity)
+  end
+
+  def empty?
+    !!(@bikes.empty?)
+  end
+
 end
 
 class Bike
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,23 +1,41 @@
 require 'docking_station'
 
 describe DockingStation do
-	it { is_expected.to respond_to :release_bike}
-#	it { respond_to?(release_bike) }
-	it 'checks bike is working' do
-		bike = subject.release_bike
+
+	#	it { respond_to?(release_bike) }
+	it '#checks bike is working' do
+		bike = Bike.new
 		expect(bike).to be_working
 	end
 
+	describe '#release_bike' do
+		it { is_expected.to respond_to :release_bike}
+		# Using a '#' before a method name implies that it is an instance method.
+		it 'raises an error when there are no bikes available' do
+			expect { subject.release_bike }.to raise_error 'No bikes available'
+	 	end
+
+		it '#returns docked bikes' do
+			bike = Bike.new
+			subject.dock(bike)
+			expect(subject.release_bike).to eq bike
+		end
+	end
+
+	describe '#dock(bike)' do
+		# expect it to raise an error when there's already a bike docked.
+		it 'raises an error when there are no empty docks' do
+			subject.capacity.times { subject.dock(Bike.new) }
+			expect { subject.dock(Bike.new) }.to raise_error 'Docking station full'
+		end
+	end
+
 	it { is_expected.to respond_to(:dock).with(1).argument }
-	it 'docks something' do
+	it '#docks something' do
 		bike = Bike.new
 		# We want to return the bike we dock
 		expect(subject.dock(bike)).to eq bike
 	end
 
-	it 'returns docked bikes' do
-		bike = Bike.new
-		subject.dock(bike)
-		expect(subject.bike).to eq bike
-	end
+
 end
